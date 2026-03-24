cfba410680c8658ea037c8e760bef2e55d3486bb
Владислав Дерієнко <derienko4@gmail.com>
2026-03-24 12:03:11 +0200

    Task1.java

--- Task1.java
+++ Task1.java
@@ -1,7 +1,7 @@
 public class Task1 {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Аргументи не передані.");
+            System.out.println("Немає аргументів командного рядка.");
         } else {
             System.out.println("Аргументи командного рядка:");
             
